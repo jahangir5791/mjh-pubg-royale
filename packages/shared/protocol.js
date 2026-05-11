db974787f0980c1ef80b9ac32a267239be6b307b
jahangir5791 <jenisaniaini@gmail.com>
2026-05-11 22:58:51 +0300

    feat(web): connect client input to multiplayer socket
    Send local input state to the server and join the default room on connect.

--- packages/shared/protocol.js
+++ packages/shared/protocol.js
@@ -1,13 +1,20 @@
-export const EVENTS = {
-  JOIN_ROOM: 'joinRoom',
-  ROOM_JOINED: 'roomJoined',
-  PLAYER_JOINED: 'playerJoined',
-  PLAYER_LEFT: 'playerLeft',
-  PLAYER_INPUT: 'playerInput',
-  PLAYER_STATE: 'playerState',
-};
+import io from 'socket.io-client';
+import { initInput, inputState } from './input.js';
+import { EVENTS, DEFAULT_ROOM } from '../../packages/shared/protocol.js';
 
-export const DEFAULT_ROOM = 'lobby';
+const socket = io('http://localhost:3000');
+initInput(canvas);
 
-export const TICK_RATE = 20;
-export const PLAYER_SPEED = 0.12;
+socket.on('connect', () => {
+  socket.emit(EVENTS.JOIN_ROOM, { roomId: DEFAULT_ROOM });
+});
+
+setInterval(() => {
+  socket.emit(EVENTS.PLAYER_INPUT, {
+    forward: inputState.forward,
+    backward: inputState.backward,
+    left: inputState.left,
+    right: inputState.right,
+    rotY: player.rotation.y,
+  });
+}, 50);
